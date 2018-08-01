76ba1a04ce76b0c5e825988b7aa0c7e0b8933063
Rishav Bhagat <rishavb123@gmail.com>
2018-08-01 12:29:08 -0400

    made slight changes to config

--- src/configurations/Constants.java
+++ src/configurations/Constants.java
@@ -7,9 +7,9 @@ package configurations;
  */
 public class Constants 
 {
-	public static final double dim = 600;
-	public static final double widthMultiplier = 2;
-	public static final double heightMultiplier = 1;
-	public static final double width = dim*widthMultiplier;
-	public static final double height = dim*heightMultiplier;
+	public static double dim = 600;
+	public static double widthMultiplier = 2;
+	public static double heightMultiplier = 1;
+	public static double width = dim*widthMultiplier;
+	public static double height = dim*heightMultiplier;
 }
