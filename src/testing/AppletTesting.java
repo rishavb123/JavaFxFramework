b70bb5593c967ead019b341299c3fedb3f7a5ffa
Rishav Bhagat <rishavb123@gmail.com>
2018-08-03 13:04:16 -0400

    testing with applets

--- src/testing/AppletTesting.java
+++ src/testing/AppletTesting.java
@@ -6,15 +6,4 @@ public class AppletTesting extends Applet{
 
 	private static final long serialVersionUID = 2959861835789684579L;
 
-	@Override
-	public void startFunc() {
-		gc.fillRect(10, 10, 100, 100);
-	}
-
-	@Override
-	public void nextFunc() {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
